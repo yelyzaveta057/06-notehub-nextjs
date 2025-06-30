4c3a8ab6022a04a7ac44a17f157c3d12bed854ed
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-06-30 13:43:37 -0400

    Update NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { useQuery } from '@tanstack/react-query'
-import { fetchNoteById } from '../../../lib/api'
-import { useParams } from 'next/navigation'
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { fetchNoteById } from "../../../lib/api";
 
-import css  from "./NoteDetails.module.css"
+import css from "./NoteDetails.module.css";
 
 const NoteDetailsClient = () => {
   const { id } = useParams<{ id: string }>();
@@ -24,16 +24,16 @@ const NoteDetailsClient = () => {
   if (error || !note) return <p className="error">Something went wrong.</p>;
 
   return (
-   <div className={css.container}>
-	<div className={css.item}>
-	  <div className={css.header}>
-	    <h2>Note title</h2>
-	    <button className={css.editBtn}>Edit note</button>
-	  </div>
-	  <p className={css.content}>Note content</p>
-	  <p className={css.date}>Created date</p>
-	</div>
-</div>
+    <div className={css.container}>
+      <div className={css.item}>
+        <div className={css.header}>
+          <h2>{note.title}</h2>
+          <button className={css.editBtn}>Edit note</button>
+        </div>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
+      </div>
+    </div>
   );
 };
 
