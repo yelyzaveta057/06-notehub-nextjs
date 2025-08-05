99b21469823e7c448b848f781dd0cc8fb7e52c7a
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-08-05 10:12:24 -0400

    add

--- lib/api.tsx
+++ lib/api.tsx
@@ -4,7 +4,6 @@ import type { Note, NewNoteData } from '../types/note';
 const BASE_URL = "https://notehub-public.goit.study/api";
 const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
-
 const noteServiceClient = axios.create({
   baseURL: BASE_URL,
   headers: {
@@ -12,40 +11,39 @@ const noteServiceClient = axios.create({
   },
 });
 
-interface FetchNotesResponse {
+interface NotesHttpResponse {
   notes: Note[];
   totalPages: number;
 }
 
 export const fetchNotes = async (
-  page = 1,
-  query = '',
-  perPage = 12
-): Promise<FetchNotesResponse> => {
-  const params = {
+  query: string,
+  page: number
+): Promise<NotesHttpResponse> => {
+  const params: Record<string, string> = {
     page: page.toString(),
-    perPage: perPage.toString(),
-    ...(query.trim() && { search: query }),
   };
 
+  if (query.trim()) {
+    params.search = query;
+  }
 
-  const res = await noteServiceClient.get<FetchNotesResponse>('/notes', { params });
-console.log("🔍 Notes:", res.data.notes);
+  const res = await noteServiceClient.get<NotesHttpResponse>('/notes', { params });
 
   return res.data;
 };
+
 export const fetchNoteById = async (id: string): Promise<Note> => {
-  const res = await noteServiceClient.get<Note>(`/note/${id}`);
+  const res = await noteServiceClient.get<Note>(`/notes/${id}`);
   return res.data;
 };
 
 export const createNote = async (noteData: NewNoteData): Promise<Note> => {
-  const res = await noteServiceClient.post<Note>('/note', noteData);
-  
+  const res = await noteServiceClient.post<Note>('/notes', noteData);
   return res.data;
 };
 
 export const deleteNote = async (noteId: string): Promise<Note> => {
-  const res = await noteServiceClient.delete<Note>(`/note/${noteId}`);
+  const res = await noteServiceClient.delete<Note>(`/notes/${noteId}`);
   return res.data;
 };
